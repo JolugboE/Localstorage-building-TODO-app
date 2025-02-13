42c7359bc0ea34db7b0041a67bf683a748288d88
JolugboE <jolugboesther@gmail.com>
2025-02-13 11:41:40 +0100

    local storage project

--- script.js
+++ script.js
@@ -47,8 +47,14 @@ if(dataArrIndex === -1){
   taskData.forEach(({id, title, date, description}) => {
       tasksContainer.innerHTML += `
 <div class="task" id="${id}">
+ <p><strong>Title:</strong>${title}</p>     
+ <p><strong>Date:</strong>${date}</p>
+ <p><strong>Description:</strong>)${description}</p> 
+<button type="button" class="btn">Edit</button>
+<button type="button" class="btn">Delete</button>
           </div>
              `
     }
   );
+  
       });
