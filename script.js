c060e83589aa1a832deca12a76f4cd46dca69324
JolugboE <jolugboesther@gmail.com>
2025-02-13 07:23:46 +0100

    local storage project

--- script.js
+++ script.js
@@ -32,5 +32,9 @@ openTaskFormBtn.addEventListener("click", () =>
     e.preventDefault();
 
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+const taskObj ={
+    id: titleInput.value.toLowerCase().split(" ")
+};
+console.log(taskObj); 
 });
-  
