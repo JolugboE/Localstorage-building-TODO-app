ebe4b889c066c0a80d1b669d10715984277f3162
JolugboE <jolugboesther@gmail.com>
2025-02-24 11:50:44 +0100

    local storage project

--- script.js
+++ script.js
@@ -120,5 +120,11 @@ const myTaskArr = [
 
   localStorage.setItem("data", JSON.stringify(myTaskArr));
 
+  localStorage.removeItem("data");
+  localStorage.clear("data");
+
   const getTaskArr = localStorage.getItem("data")
 console.log(getTaskArr)
+
+const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+console.log(getTaskArrObj);
